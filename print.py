480410f15757122554c5ba0d00d7a62f09487b8e
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 09:04:32 +0600

    Create print.py

---
+++ print.py
@@ -0,0 +1,6 @@
+print("hello world")
+print("this is imrul kayes ")
+print("i study for being well educated")
+print("this is my first program")
+print("i love Bangladesh")
+print("and love this City")
